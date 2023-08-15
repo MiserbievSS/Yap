de08dde44dbdf282f6204612c054fb4e72a30130
Sergei Miserbiev <miserbievss@MacBook-Pro-Sergei.local>
2023-08-15 13:18:44 +0300

    deploy1

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-r-oi=b%^mu_45rj+5yjs0a=yv+s8)nt0^*1@=sj51_(1*bh2f@'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False')
+DEBUG = True
 
 ALLOWED_HOSTS = ['158.160.66.8', 'foodgrammss.ddns.net']
 
