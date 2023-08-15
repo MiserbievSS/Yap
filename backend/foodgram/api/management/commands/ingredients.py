d8e7ee217bc8242ec0832cbe4b6720c1c3fe77c6
Sergei Miserbiev <miserbievss@MacBook-Pro-Sergei.local>
2023-08-15 14:35:09 +0300

    deploy1

--- backend/foodgram/api/management/commands/ingredients.py
+++ backend/foodgram/api/management/commands/ingredients.py
@@ -10,7 +10,8 @@ from recipes.models import Ingredient
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         with open(
-            os.path.join(os.path.join(settings.BASE_DIR, 'data'), 'ingredients.json'), 'r'
+            os.path.join(os.path.join(settings.BASE_DIR, 'data'),
+                         'ingredients.json'), 'r'
         ) as file:
             ingredients = json.load(file)
             for ingredient in ingredients:
