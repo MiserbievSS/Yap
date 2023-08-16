567d9292cb8d74bff9ec91008f54c1f134752521
Sergei Miserbiev <miserbievss@MacBook-Pro-Sergei.local>
2023-08-16 19:40:54 +0300

    deploy1

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -11,13 +11,13 @@ router.register('ingredients', IngredientViewSet)
 router.register('recipes', RecipeViewSet)
 
 
-#urlpatterns = [
+# urlpatterns = [
 #    path('auth/', include('djoser.urls.authtoken')),
 #    path('', include(router.urls)),
-#]
+# ]
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
     path('api/', include('djoser.urls')),  # Работа с пользователями
     path('api/', include('djoser.urls.authtoken')),  # Работа с токенами
 ]
