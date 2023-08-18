45bb3451280dbcdf7edd53585da819c3735dfcee
Sergei Miserbiev <mamayabolen@gmail.com>
2023-08-18 21:46:47 +0300

    deploy finish

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -9,6 +9,14 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=150)
     is_subscribed = models.BooleanField(default=False)
 
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
+
 
 class Subscription(models.Model):
     user = models.ForeignKey(
