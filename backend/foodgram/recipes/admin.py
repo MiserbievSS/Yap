4400dc61b5325deaf48637f3760ef4274d4250ad
Sergei Miserbiev <mamayabolen@gmail.com>
2023-08-19 09:57:02 +0300

    verbose

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.admin import display
 
 from recipes.models import (FavoriteList, Ingredient, Recipe, RecipeIngredient,
                             ShoppingCart, Tag)
@@ -16,7 +17,13 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    inlines = (RecipeIngredientInline, )
+    list_display = ('name', 'id', 'author', 'added_in_favorites')
+    readonly_fields = ('added_in_favorites',)
+    list_filter = ('author', 'name', 'tags',)
+
+    @display(description='Количество в избранных')
+    def added_in_favorites(self, obj):
+        return obj.favorites.count()
 
 
 @admin.register(Ingredient)
--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -13,13 +13,17 @@ class Tag(models.Model):
         verbose_name = 'Тег'
         verbose_name_plural = 'Теги'
 
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     tags = models.ManyToManyField(Tag, related_name='recipes')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='recipes'
+        related_name='recipes',
+        verbose_name='Автор',
     )
     ingredients = models.ManyToManyField(
         'Ingredient',
@@ -36,6 +40,9 @@ class Recipe(models.Model):
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
 
+    def __str__(self):
+        return self.name
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=200)
@@ -45,46 +52,66 @@ class Ingredient(models.Model):
         verbose_name = 'Ингредиент'
         verbose_name_plural = 'Ингредиенты'
 
+    def __str__(self):
+        return f'{self.name}, {self.measurement_unit}'
+
 
 class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(
         Recipe,
         related_name='recipe_ingredients',
+        verbose_name='Рецепт',
         on_delete=models.CASCADE
     )
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     amount = models.PositiveIntegerField()
+
+    def __str__(self):
+        return (
+            f'{self.ingredient.name}'
+            f'({self.ingredient.measurement_unit}) - {self.amount}'
+        )
 
 
 class FavoriteList(models.Model):
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='is_favorited'
+        related_name='is_favorited',
+        verbose_name='Пользователь',
     )
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='is_favorited'
+        related_name='is_favorited',
+        verbose_name='Рецепт',
     )
 
     class Meta:
         verbose_name = 'Избранное'
         verbose_name_plural = 'Избранные'
 
+    def __str__(self):
+        return f'{self.user} добавил "{self.recipe}" в Избранное'
+
 
 class ShoppingCart(models.Model):
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='is_in_shopping_cart'
+        related_name='is_in_shopping_cart',
+        verbose_name='Пользователь',
     )
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='is_in_shopping_cart'
+        related_name='is_in_shopping_cart',
+        verbose_name='Рецепт',
     )
 
     class Meta:
         verbose_name = 'Корзина'
         verbose_name_plural = 'Корзины'
+
+    def __str__(self):
+        return f'{self.user} добавил "{self.recipe}" в корзину'
